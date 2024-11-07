b5d038829dc023958ec4173d294dc62fa9758ea3
Ayden Jahola <jaholaayden@gmail.com>
2024-11-07 11:03:19 +0000

    nvim: use official copilot plugin and add mappings for completion

--- .config/nvim/lua/custom/mappings.lua
+++ .config/nvim/lua/custom/mappings.lua
@@ -12,11 +12,22 @@ M.general = {
       end,
       "formatting",
     }
-
   },
   v = {
     [">"] = { ">gv", "indent"},
   },
+}
+
+M.copilot = {
+  i = {
+    ["<C-l>"] = {
+      function()
+        vim.fn.feedkeys(vim.fn['copilot#Accept'](), '')
+      end,
+      "Copilot Accept",
+       {replace_keycodes = true, nowait=true, silent=true, expr=true, noremap=true}
+      }
+  }
 }
 
 -- more keybinds!
--- .config/nvim/lua/custom/plugins.lua
+++ .config/nvim/lua/custom/plugins.lua
@@ -48,14 +48,16 @@ local plugins = {
   },
 
   {
-    "zbirenbaum/copilot.lua",
-    event = "InsertEnter",
-    cmd = "Copilot",
-    opts = {
-      suggestion = {
-        auto_trigger = true,
-      }
-    }
+    "github/copilot.vim",
+    url = "git@github.com:github/copilot.vim.git",
+    lazy = false,
+    config = function()  -- Mapping tab is already used by NvChad
+      vim.g.copilot_no_tab_map = true;
+      vim.g.copilot_assume_mapped = true;
+      vim.g.copilot_tab_fallback = "";
+    -- The mapping is set to other key, see custom/lua/mappings
+    -- or run <leader>ch to see copilot mapping section
+    end
   },
 
   -- To make a plugin not be loaded
